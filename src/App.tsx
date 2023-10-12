13fc666a4cd99f99cb072e0f91045498e04560ee
Rich Kevan <rskevan@me.com>
2023-10-12 06:40:12 -0700

    Updated gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import insta from './assets/2227.png';
+import {ReactComponent as Download} from './assets/download.svg'
 
 function App() {
 
@@ -7,9 +8,14 @@ function App() {
       <div className='flex h-64 aspect-square mx-auto'>
         <img src={insta} alt="insta" />
       </div>
-      <div className='mx-auto'>
-        <form>
-          <input type='text' placeholder='Paste Instagram URL' />
+      <div className='mx-auto rounded-full shadow-2xl p-4 w-1/3'>
+        <form className='flex'>
+          <input 
+          type='text' 
+          placeholder='Paste Instagram URL' 
+          className='flex-1  text-lg font-semibold focus:outline-0'/>
+          <span className='border-2 border-black mx-4'></span>
+          <button type='submit'><Download className=' w-8 h-8'/></button>
         </form>
       </div>
     </div>
