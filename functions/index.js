281d60d9823f1fa829046a9366e7aa41b09bcfa3
Rich Kevan <rskevan@me.com>
2023-10-12 13:04:18 -0700

    Automated deploy

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,60 @@
-const functions = require("firebase-functions");
-const axios = require("axios");
+// const functions = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios").default;
 const cheerio = require("cheerio");
 
-exports.instaGrab = functions.https.onCall(async (data, context) => {
-  functions.logger.info(data);
-  const html = await axios(data.url);
-  functions.logger.info(html);
+// exports.instaGrab = functions.https.onCall(async (data, context) => {
+//   functions.logger.info(data);
+//   const html = await axios(data.url);
+//   functions.logger.info(html);
+//   const $ = cheerio.load(html.data);
+//   functions.logger.info("Cheerio Loaded html: ", $);
+//   var imageString;
+//   if (data.radio === "image") {
+//     imageString = $("meta[property='og:image']").attr("content");
+//     functions.logger.info("Instagram Image URL: ", imageString);
+//   }
+//   if (data.radio === "video") {
+//     imageString = $("script[nonce='od0scHEf']").attr("contentUrl");
+//     functions.logger.info("Instagram Video URL: ", imageString);
+//   }
+//   return { type: data.radio, url: imageString };
+// });
+
+const app = express();
+
+const corsOptions = {
+  'Access-Control-Allow-Origin': '*',
+  'Methods': ['GET','OPTION'],
+};
+
+app.use(cors(corsOptions)); 
+
+const imageDownload = app.get("/image", async (request, response) => {
+  logger.info(request.query.url);
+  const html = await axios(request.query.url);
+  logger.info(html.data);
   const $ = cheerio.load(html.data);
-  functions.logger.info("Cheerio Loaded html: ", $);
   var imageString;
-  if (data.radio === "image") {
     imageString = $("meta[property='og:image']").attr("content");
-    functions.logger.info("Instagram Image URL: ", imageString);
-  }
-  if (data.radio === "video") {
-    imageString = $("script[nonce='od0scHEf']").attr("contentUrl");
-    functions.logger.info("Instagram Video URL: ", imageString);
-  }
-  return { type: data.radio, url: imageString };
+    response.header("Access-Control-Allow-Origin", "*", "Content-Type", "text/plain");
+  response.status(200).json({src:imageString});
+})
+
+const videoDownload = app.get("/video", async (request, response) => {
+  logger.info(request.query.url);
+  const html = await axios(request.query.url);
+  logger.info(html.data);
+  const $ = cheerio.load(html.data);
+  var imageString;
+    imageString = $("meta[property='og:video']").attr("content");
+    response.header("Access-Control-Allow-Origin", "*", "Content-Type", "text/plain");
+  logger.info(imageString);
+  response.status(200).json({src:imageString});
 });
+
+exports.instaDownloadImage = onRequest(imageDownload);
+exports.instaDownloadVideo = onRequest(videoDownload);
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,67 @@
+import { useState } from 'react';
 import insta from './assets/2227.png';
 import {ReactComponent as Download} from './assets/download.svg'
 
 function App() {
+  const [src, setSrc] = useState("");
+  const [media] = useState("img");
+
+  const fetchImage = async (e: { preventDefault: () => void; target: HTMLFormElement | undefined; }) => {
+    e.preventDefault()
+    const entry = new FormData(e.target);
+    const data = Object.fromEntries(entry);
+    const params = new URLSearchParams(data.url.toString());
+    if (media === "img") {
+    fetch(`https://instadownloadimage-mr2xxcosrq-uc.a.run.app/image?url=`+ params)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setSrc(data.src)
+    })
+  } 
+  // else if (media === "video") {
+  //   fetch(`https://instadownloadvideo-mr2xxcosrq-uc.a.run.app/video?url=`+ params)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setSrc(data.src)
+  //   })
+  // }
+  };
 
   return (
     <div className="flex flex-col pt-16 gap-8">
       <div className='flex h-64 aspect-square mx-auto'>
         <img src={insta} alt="insta" />
       </div>
+      {/* <div className='flex mx-auto rounded-full shadow-2xl w-1/3 bg-gradient-to-r from-[#0348dd] from-5% via-[#8142f5] via-30% to-[#ee4dd4] to-95% backdrop-blur-lg text-white font-semibold'>
+        <div 
+        className='flex-1 text-center  p-4 cursor-pointer' 
+        onClick={() => {setMedia('video')}}>Video</div>
+        <span className='border-2 border-black mx-4'></span>
+        <div 
+        className='flex-1 text-center  p-4 cursor-pointer'
+        onClick={() => {setMedia('img')}}>
+        Photo</div>
+      </div> */}
       <div className='mx-auto rounded-full shadow-2xl p-4 w-1/3'>
-        <form className='flex'>
+        <form className='flex' onSubmit={fetchImage}>
           <input 
-          type='text' 
+          type='text'
+          name='url'
           placeholder='Paste Instagram URL' 
           className='flex-1  text-lg font-semibold focus:outline-0'/>
           <span className='border-2 border-black mx-4'></span>
           <button type='submit'><Download className=' w-8 h-8'/></button>
         </form>
+      </div>
+      <div className='h-96'>
+        {src && media ?
+        (media === "img" &&
+        <img src={src} alt="insta" className='mx-auto h-full w-auto'/>) : (
+        media === "video" &&
+        <video src={src} className='mx-auto h-full w-auto'/>
+        )}
       </div>
     </div>
   )
